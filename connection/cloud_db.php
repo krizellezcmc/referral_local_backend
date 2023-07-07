bb1b50b2e80fbd767aa3c1787c0761713a093544
Krizelle Mae Falcasantos - ZCMC <kmfalcasantos.zcmc@gmail.com>
2023-07-07 14:02:49 +0800

    asd

--- connection/cloud_db.php
+++ connection/cloud_db.php
@@ -3,7 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: *");
 
-$cloud_db = mysqli_connect("sql624.main-hosting.eu", "u305484025_zcmc_referral", "Zcmc_referral123","u305484025_zcmc_referral");
+$cloud_db = mysqli_connect("sql281.main-hosting.eu", "u305484025_zcmc_referral", "Zcmc_referral123","u305484025_zcmc_referral");
 
 if($cloud_db === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
